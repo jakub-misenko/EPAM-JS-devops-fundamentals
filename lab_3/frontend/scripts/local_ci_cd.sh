113951e751596ad400bba7ab1ca3b9390157904f
Jakub Misenko <Jakub_Misenko@epam.com>
2022-10-30 22:04:27 +0100

    fixed missing src, switched scp for rsynx

--- lab_3/frontend/scripts/local_ci_cd.sh
+++ lab_3/frontend/scripts/local_ci_cd.sh
@@ -3,9 +3,8 @@
 cd ..
 npm audit
 npm run test
-npm run build
 
 # source ./build -- not working
 npm run build
 
-scp -r ./dist/app sshuser@192.168.128.2:/var/www/app
+rsync -aP ./dist/app sshuser@192.168.128.2:/var/www/html
